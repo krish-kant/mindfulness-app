13fa3daacaec3ff85cb4783cef4a24bb05b87283
krishna-kant0 <krishna.200587@gmail.com>
2022-11-16 14:06:42 +0530

    added navigation tabs

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,19 +4,25 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+           <ion-icon :icon="homeOutline"/>
+          <ion-label>home</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon :icon="libraryOutline"/>
+          <ion-label>library</ion-label>
         </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon :icon="flagOutline" />
+          <ion-label>goals</ion-label>
         </ion-tab-button>
+
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon :icon="personOutline" />
+          <ion-label>profile</ion-label>
+        </ion-tab-button>
+
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -25,6 +31,6 @@
 <script setup>
 
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { homeOutline, flagOutline, libraryOutline, personOutline } from 'ionicons/icons';
 
 </script>
---
+++ src/views/libraryPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <ion-page>
+   
+    <ion-content :fullscreen="true">
+
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup>
+import { IonPage, IonContent } from '@ionic/vue';
+
+
+</script>
