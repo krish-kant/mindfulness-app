0434ffc878053b8fabaddfc45a0a099cbd42bdb0
krishna-kant0 <krishna.200587@gmail.com>
2022-11-17 16:40:10 +0530

    updated navigation items

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,33 +1,75 @@
 <template>
+
   <ion-page>
+
     <ion-header>
+
       <ion-toolbar>
-        <ion-title>Home</ion-title>
+        <!-- <ion-title>Home</ion-title> -->
+
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Hi Krishna</ion-title>
+
+
         </ion-toolbar>
       </ion-header>
-      <ion-card>
-    <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
-    <ion-card-header>
-      <!-- <ion-card-title>Card Title</ion-card-title> -->
-      <!-- <ion-card-subtitle>Card Subtitle</ion-card-subtitle> -->
-    </ion-card-header>
 
-    <!-- <ion-card-content>
-      Here's a small text description for the card content. Nothing more, nothing less.
-    </ion-card-content> -->
-  </ion-card>
-  <ion-button class="ion-margin-start" fill="solid" size="large" >Solid</ion-button>
-  <ion-button class="ion-margin-start" fill="solid" size="large" >Solid</ion-button>
-  <ion-button class="ion-margin-start" fill="solid" size="large" >Solid</ion-button>
-  <ion-button class="ion-margin-start" fill="solid" size="large" >Solid</ion-button>
-  <ion-button class="ion-margin-start" fill="solid" size="large" >Solid</ion-button>
-  <ion-button class="ion-margin-start" fill="solid" size="large" >Solid</ion-button>
+      <ion-grid class="ion-margin-top">
+        <ion-row>
+          <ion-col size="6" size-sm="4" size-lg="3" v-for="n in items" :key="n.message">
+            <ion-card class="card-items">
+
+              <ion-card-header>
+
+                <ion-item lines="none">
+
+                  <ion-icon :icon="time" class="ion-padding-end"></ion-icon>
+                  <ion-label>{{ n.message }}</ion-label>
+                </ion-item>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-grid class="ion-margin-top">
+        <ion-item lines="none">
+          <ion-text color="">
+            <h3>Items</h3>
+          </ion-text>
+        </ion-item>
+        <ion-row class="ion-justify-content-center">
+          <ion-col size="12" size-sm="6" size-lg="4" v-for="n in items" :key="n.message">
+            <ion-card>
+              <div class="container">
+                <div class="tag">
+                  <ion-icon :icon="lockClosed"></ion-icon>
+                </div>
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+              </div>
+              <ion-card-header>
+
+                <ion-item lines="none">
+                  <ion-label>{{ n.message }}</ion-label>
+                  <ion-icon :icon="bookmarkOutline" slot="end"></ion-icon>
+                </ion-item>
+              </ion-card-header>
+              <!-- 
+              <ion-card-content>
+                Here's a small text description for the card content. Nothing more, nothing less.
+              </ion-card-content> -->
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+
+
+
 
     </ion-content>
   </ion-page>
@@ -35,14 +77,55 @@
 
 <script setup>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/vue";
+import { time, lockClosed, bookmarkOutline } from 'ionicons/icons';
+import { ref } from "vue";
+
+
+const items = ref([{ message: 'Yoga' }, { message: 'Meditation' }, { message: 'Breethe' }, { message: 'Timer' }, { message: 'SOS' }, { message: 'Sleep' }])
 
 
 </script>
 
 <style scoped>
-  /* iOS places the subtitle above the title */
-  ion-card-header.ios {
-    display: flex;
-    flex-flow: column-reverse;
-  }
+/* iOS places the subtitle above the title */
+ion-card-header.ios {
+  display: flex;
+  flex-flow: column-reverse;
+}
+
+
+
+.card-items {
+
+  text-align: center;
+  margin: 6px;
+  padding: 8px;
+
+}
+
+
+
+
+
+ion-card-header {
+  padding: 2px !important;
+
+}
+
+
+.container {
+  position: relative;
+}
+
+.tag {
+  float: right;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  z-index: 1000;
+  /* background-color: #e3c346; */
+  padding: 5px;
+  color: #ebad1c;
+  font-weight: bold;
+}
 </style>
