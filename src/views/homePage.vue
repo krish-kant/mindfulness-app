0b5bf1f0cf4554e738e5e5f7e9b8726e889b7be8
krishna-kant0 <krishna.200587@gmail.com>
2022-11-17 23:45:03 +0530

    added android

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,20 +1,14 @@
 <template>
-
   <ion-page>
-
     <ion-header>
-
       <ion-toolbar>
         <!-- <ion-title>Home</ion-title> -->
-
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Hi Krishna</ion-title>
-
-
         </ion-toolbar>
       </ion-header>
 
@@ -22,11 +16,8 @@
         <ion-row>
           <ion-col size="6" size-sm="4" v-for="n in items" :key="n.message">
             <ion-card class="card-items">
-
               <ion-card-header>
-
                 <ion-item lines="none">
-
                   <ion-icon class="icon-item" :icon="time"></ion-icon>
                   <ion-label>{{ n.message }}</ion-label>
                 </ion-item>
@@ -43,15 +34,23 @@
           </ion-text>
         </ion-item>
         <ion-row class="ion-justify-content-start">
-          <ion-col size="6" size-sm="3" v-for="n in recentlyPlayed" :key="n.message">
+          <ion-col
+            size="6"
+            size-sm="3"
+            v-for="n in recentlyPlayed"
+            :key="n.message"
+          >
             <ion-card>
-              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+              <img
+                alt="Silhouette of mountains"
+                src="https://ionicframework.com/docs/img/demos/card-media.png"
+              />
 
               <ion-card-header class="recently-played-header">
-
                 <ion-item lines="none">
-                  <ion-text class="recently-played-text">{{ n.message }}</ion-text>
-
+                  <ion-text class="recently-played-text">{{
+                    n.message
+                  }}</ion-text>
                 </ion-item>
               </ion-card-header>
               <!-- 
@@ -70,16 +69,24 @@
           </ion-text>
         </ion-item>
         <ion-row class="ion-justify-content-start">
-          <ion-col size="12" size-sm="6" size-lg="4" v-for="n in recentlyPlayed" :key="n.message">
+          <ion-col
+            size="12"
+            size-sm="6"
+            size-lg="4"
+            v-for="n in recentlyPlayed"
+            :key="n.message"
+          >
             <ion-card>
               <div class="container">
                 <div class="tag">
                   <ion-icon :icon="lockClosed"></ion-icon>
                 </div>
-                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                <img
+                  alt="Silhouette of mountains"
+                  src="https://ionicframework.com/docs/img/demos/card-media.png"
+                />
               </div>
               <ion-card-header>
-
                 <ion-item lines="none">
                   <ion-label>{{ n.message }}</ion-label>
                   <ion-icon :icon="bookmarkOutline" slot="end"></ion-icon>
@@ -93,26 +100,38 @@
           </ion-col>
         </ion-row>
       </ion-grid>
-
-
-
-
-
     </ion-content>
   </ion-page>
 </template>
 
 <script setup>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/vue";
-import { time, lockClosed, bookmarkOutline } from 'ionicons/icons';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import { time, lockClosed, bookmarkOutline } from "ionicons/icons";
 import { ref } from "vue";
 
+const items = ref([
+  { message: "Yoga" },
+  { message: "Meditation" },
+  { message: "Breethe" },
+  { message: "Timer" },
+  { message: "SOS" },
+  { message: "Sleep" },
+]);
 
-const items = ref([{ message: 'Yoga' }, { message: 'Meditation' }, { message: 'Breethe' }, { message: 'Timer' }, { message: 'SOS' }, { message: 'Sleep' }])
-
-const recentlyPlayed = ref([{ message: 'Yoga and mindfulness' }, { message: 'Meditation and yoga' }, { message: 'Breethe' }, { message: 'Timer guided' }, { message: 'Yoga and mindfulness' }, { message: 'Meditation and yoga' }])
-
-
+const recentlyPlayed = ref([
+  { message: "Yoga and mindfulness" },
+  { message: "Meditation and yoga" },
+  { message: "Breethe" },
+  { message: "Timer guided" },
+  { message: "Yoga and mindfulness" },
+  { message: "Meditation and yoga" },
+]);
 </script>
 
 <style scoped>
@@ -122,22 +141,16 @@ ion-card-header.ios {
   flex-flow: column-reverse;
 }
 
-
-
 .card-items {
-
   text-align: center;
   margin: 6px;
   padding-top: 8px;
   padding-bottom: 8px;
-
 }
 
 ion-card-header {
   padding: 2px !important;
-
 }
-
 
 .container {
   position: relative;
